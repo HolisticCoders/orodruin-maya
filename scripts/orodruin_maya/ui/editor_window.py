c5cffec6304d3b53686aae2cb1a8899d58565d37
Loïc Pinsard <muream@gmail.com>
2021-09-21 23:07:02 +0200

    use root graph instead of root component

--- scripts/orodruin_maya/ui/editor_window.py
+++ scripts/orodruin_maya/ui/editor_window.py
@@ -1,4 +1,4 @@
-from orodruin.core import Component
+from orodruin.core import Graph
 from orodruin_editor.gui.editor_window import OrodruinEditorWindow
 from orodruin_maya.core import OMGraph
 from PySide2.QtWidgets import QVBoxLayout
@@ -19,6 +19,7 @@ class OrodruinMayaWindow(DockableWidget):
         self.orodruin_editor = OrodruinEditorWindow()
         self.layout.addWidget(self.orodruin_editor)
 
-        self.root_component = Component("root")
-        self.om_graph = OMGraph(self.root_component.graph())
-        self.orodruin_editor.set_active_scene(self.root_component)
+        self.root_graph = Graph()
+        self.om_graph = OMGraph(self.root_graph)
+        self.orodruin_editor.graphs[self.root_graph.uuid()] = self.root_graph
+        self.orodruin_editor.set_active_scene(self.root_graph.uuid())
